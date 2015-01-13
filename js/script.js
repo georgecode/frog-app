410614c1ed2cff153180bce6bc614f177cebc3ad
georgecode <georgeczurlanis@gmail.com>
2015-01-12 21:05:04 -0500

    a working pop up finally

--- js/script.js
+++ js/script.js
@@ -7,18 +7,18 @@ $(document).ready(function(){
 		//$('#xxclose').html('<p class="xxclose">X</p>');
 		//$('#info').html('<p class="popuptext">Your bones dont break, mine do. Thats clear. Your cells react to bacteria and viruses differently than mine. You dont get sick, I do. Thats also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. Were on the same curve, just on opposite ends.</p>')
 		//$('.row').fadeTo('slow', .25);
-		//$('#popup').html('<iframe src="http://www.mywalldrywall.com/" frameborder="0" scrolling="no" id="myFrame"></iframe>');
-		//$('#popup').addClass('moveUp')
-		$('#popup').html(
-			<div class="panel panel-default">
-    			<div class="panel-heading">
-          			<h3 class="panel-title">X</h3>
-        		</div>
-        		<div class="panel-body">
-          			Panel content
-        		</div>
-      		</div>
-      	);
+		$('#popup').html('<iframe src="http://www.mywalldrywall.com/" frameborder="0" scrolling="no" id="myFrame"></iframe>');
+		$('#popup').addClass('moveUp')
+		//$('#popup').html(
+		//	<div class="panel panel-default">
+    	//		<div class="panel-heading">
+          //			<h3 class="panel-title">X</h3>
+        	//	</div>
+        	//	<div class="panel-body">
+          	//		Panel content
+        	//	</div>
+      		//</div>
+      	//);
 	});
 
 	//$('#xxclose').click(function(){
