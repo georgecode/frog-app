763d4079573a1259c43c71b5bb4a559f4dbc94e0
georgecode <georgeczurlanis@gmail.com>
2015-01-23 02:10:48 -0500

    play button working frog sound starting and stoping and perfect opasity...awsome

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-	var silent = '<audio autoplay> <source src="#" type="audio/wav"></audio>'
 
-	var froggy = '<audio autoplay> <source src="http://www.state.nj.us/dep/fgw/ensp/audio/bullfrog.wav" type="audio/wav"></audio>' 					   
+
+
+	var audio = new Audio('http://www.state.nj.us/dep/fgw/ensp/audio/bullfrog.wav');
+ 	
+
 	$('#triangle-right').click(function() {
-    $(this).html(froggy);
-
+  		if (audio.paused == false) {
+      		audio.pause();
+      			
+  		} else {
+      		audio.play();
+      		
+  		}
 	});
 	
 });
