eecfb7cbc1fcf69e6f63803012abfaddc9d74771
georgecode <georgeczurlanis@gmail.com>
2015-01-28 02:38:55 -0500

    multible frog modals working with indvidual sounds and info...nexst step re-organise the javascript

--- js/script.js
+++ js/script.js
@@ -22,21 +22,32 @@ $(document).ready(function(){
 }
 
 // testing calling a property from an object
-var frankFrog = new frog ("Frank", "alpha-frank", "images/Greenfrog1.jpg", "info")
+var frankFrog = new frog (
+	"george of the jungle", 
+	"alpha-frank", 
+	"images/Greenfrog1.jpg", 
+	"info")
+
+
 $('#title').append("<div>"+frankFrog.name+"</div>");	
 
 
 //testing real class
-var zAmericanBullfrog = new frog ("American bullfrog", "(Rana catesbeiana)", "images/Americanbullfrog1.jpg","bla bla bla bla bla");
+var zAmericanBullfrog = new frog ("American bullfrog", "(Rana catesbeiana)", "images/Carpenterfrog1.jpg","bla bla bla bla bla");
 
 //test turning modal in to a jquary event
 //$('#frog-contain').click(function(){
 	
 	//$('#kingModal').html('<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" id="closeStyle" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button><h4 class="modal-title" id="myModalLabel">Street Name</h4></div><div class="modal-body"><div id="workFucker"><p class="popSci-Name">(Sci-Name)</p><div class="frogpic"><img id="Americanbullfrog"src="images/Americanbullfrog1.jpg"><div id="play-button"><div id="triangle-right"></div></div></div><p class="info">This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like theyre actually proud of that shit.</p><div id="Test123"></div></div></div></div></div></div>');
 
+
+
+
+//-----------------Below is the working java script dont fuck with it	
+
 //turining modal into function
-var modalPop = function(name, sciName, id, pic, info){
-	return('<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" id="closeStyle" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button><h4 class="modal-title" id="myModalLabel">'+name+'</h4></div><div class="modal-body"><div id="workFucker"><p class="popSci-Name">('+sciName+')</p><div class="frogpic"><img id="'+id+'"src="'+pic+'"><div id="play-button"><div id="triangle-right"></div></div></div><p class="info">'+info+'This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like theyre actually proud of that shit.</p><div id="Test123"></div></div></div></div></div></div>');
+var modalPop = function(name, sciName, imgId, pic, info){
+	return('<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" id="closeStyle" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">X</span></button><h4 class="modal-title" id="myModalLabel">'+name+'</h4></div><div class="modal-body"><div id="workFucker"><p class="popSci-Name">('+sciName+')</p><div class="frogpic"><img id="'+imgId+'"src="'+pic+'"><div id="play-button"><div id="triangle-right"></div></div></div><p class="info">'+info+'This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like theyre actually proud of that shit.</p><div id="Test123"></div></div></div></div></div></div>');
 };
 
 var playSound = function(sound){
@@ -60,14 +71,38 @@ var playSound = function(sound){
 	};
 
 
+//calling the functions with a click event
 
+//---name, sciName, imgId, pic, info---
 
-$('#frog-contain').click(function(){
+$('#Americanbullfrog').click(function(){
 	
-	$('#kingModal').html(modalPop("froggy", "hydra jumppy", "Americanbullfrog", "images/Americanbullfrog1.jpg","its a froggedy frog"  ));
+	$('#kingModal').html(modalPop("American bullfrog", "Rana catesbeiana", "Americanbullfrog", "images/Americanbullfrog1.jpg","its a froggedy frog"  ));
 	return(playSound('http://www.state.nj.us/dep/fgw/ensp/audio/bullfrog.wav'));
 
 
+
+});
+
+$('#Americantoad').click(function(){
+	
+	$('#kingModal').html(modalPop("American toad", "Bufo americanus", "Americanbullfrog", "images/Americantoad1.jpg","its a froggedy frog"  ));
+	return(playSound('http://www.state.nj.us/dep/fgw/ensp/audio/american_toad.wav'));
+
+
+
+});
+
+
+
+
+}); //This is THE END
+//frog 2
+/*---$('#Americantoad').click(function(){
+	$('#kingModal').empty();
+	
+	$('#kingModal').html(modalPop("mAmericantoad","froggy", "hydra jumppy", "Americanbullfrog", "images/Americanbullfrog1.jpg","its a froggedy frog"  ));
+	return(playSound('http://www.state.nj.us/dep/fgw/ensp/audio/bullfrog.wav'));---*/
 
 
 //audio below
@@ -93,6 +128,5 @@ $('#frog-contain').click(function(){
 
 
 
-});
-});
+
 
